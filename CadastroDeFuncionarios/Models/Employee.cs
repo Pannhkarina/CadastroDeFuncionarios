fe8332663d17b7a80a084b48e82d4952b8a198f4
Pam Canale <Pam Canale@DESKTOP-7LI3NJJ>
2022-05-22 11:55:37 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroDeFuncionarios/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CadastroDeFuncionarios.Models
+{
+    public class Employee
+    {
+       [Key]
+        public int EmployeeId { get; set; }
+
+       [Column(TypeName="nvarchar(250)")]
+       [DisplayName("Nome")]
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+
+        public string FullName { get; set; }
+
+       [Column(TypeName = "varchar(50)")]
+       [DisplayName("Matricula")]
+        public string EmpCode { get; set; }
+
+       [Column(TypeName = "varchar(100)")]
+       [DisplayName("Cargo")]
+        public string Position { get; set; }
+
+       [Column(TypeName = "varchar(100)")]
+       [DisplayName("Localização")]
+        public string OfficeLocation { get; set; }
+
+    }
+}
